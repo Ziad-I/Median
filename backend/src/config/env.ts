aed4bab0ef03ca18bdc640da0a8257defb598c7f
Ziad-I <68874104+Ziad-I@users.noreply.github.com>
2024-08-31 12:33:55 +0300

    add db.ts and env.ts config files

---
+++ backend/src/config/env.ts
@@ -0,0 +1,13 @@
+// src/config/env.ts
+import dotenv from 'dotenv';
+import path from 'path';
+
+// Load environment variables from .env file
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+
+export const ENV = {
+    PORT: process.env.PORT || 3000,
+    MONGODB_URI: process.env.MONGODB_URI || 'MONGODB_URI',
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    CLOUDINARY_URL: process.env.CLOUDINARY_URL || "CLOUDINARY_URL"
+};
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,15 @@
 import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
-
-dotenv.config();
+import connectDB from './config/db';
+import { ENV } from './config/env';
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+connectDB();
+
+app.listen(ENV.PORT, () => {
+  console.log(`[server]: Server is running at http://localhost:${ENV.PORT}`);
 });
