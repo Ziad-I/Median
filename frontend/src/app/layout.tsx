4c4a979efff888bccd7083e42bf5c477c7201cfe
Ziad-I <68874104+Ziad-I@users.noreply.github.com>
2024-09-14 01:48:31 +0300

    chore move provider to own folder
    preparing to add zustard providers for state management

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,16 +1,17 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Providers } from "@/components/Providers";
+import { Provider as ThemeProvider } from "@/providers/ThemeProviders";
 import Header from "@/components/Home/Header";
 import Footer from "@/components/Home/Footer";
-import { Toaster } from "@/components/ui/toaster"
+import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Median",
-  description: "Discover stories, thinking, and expertise from writers on any topic.",
+  description:
+    "Discover stories, thinking, and expertise from writers on any topic.",
 };
 
 export default function RootLayout({
@@ -21,14 +22,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>
+        <ThemeProvider>
           <div className="flex flex-col min-h-screen ">
             <Header />
             <main className="flex-1">{children}</main>
             <Toaster />
             <Footer />
           </div>
-        </Providers>
+        </ThemeProvider>
       </body>
     </html>
   );
--- frontend/src/providers/ThemeProviders.tsx
+++ frontend/src/providers/ThemeProviders.tsx
@@ -2,6 +2,6 @@
 
 import { ThemeProvider } from "next-themes";
 
-export function Providers({ children }: { children: React.ReactNode }) {
+export function Provider({ children }: { children: React.ReactNode }) {
   return <ThemeProvider attribute="class">{children}</ThemeProvider>;
 }
