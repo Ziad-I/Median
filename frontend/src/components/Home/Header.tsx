1bfe7a71f7cca01ef8af7ab6f172457a41d54001
Ziad-I <68874104+Ziad-I@users.noreply.github.com>
2024-09-14 01:22:12 +0300

    add dashboard button if user is logged in

--- frontend/src/components/Home/Header.tsx
+++ frontend/src/components/Home/Header.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import Link from "next/link";
-import { Feather } from "lucide-react";
+import { Feather, User } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import ThemeToggle from "./ThemeToggle";
 
-const Header = () => (
+interface HeaderProps {
+  isLoggedIn: boolean;
+}
+
+const Header = ({ isLoggedIn }: HeaderProps) => (
   <header className="px-4 lg:px-6 h-14 flex items-center border-b">
     <Link className="flex items-center justify-center" href="/">
       <Feather className="h-6 w-6 text-primary" />
@@ -26,21 +30,32 @@ const Header = () => (
       </Link>
       <Link
         className="text-sm font-medium hover:underline underline-offset-4"
-        href="articles/write"
+        href="/articles/write"
       >
         Write
       </Link>
-      <Link
-        className="text-sm font-medium hover:underline underline-offset-4"
-        href="/login"
-      >
-        Sign In
-      </Link>
+      {!isLoggedIn && (
+        <Link
+          className="text-sm font-medium hover:underline underline-offset-4"
+          href="/login"
+        >
+          Sign In
+        </Link>
+      )}
     </nav>
     <ThemeToggle />
-    <Button className="ml-4" variant="outline" asChild>
-      <Link href="/signup">Get Started</Link>
-    </Button>
+    {isLoggedIn ? (
+      <Button className="ml-4" variant="outline" asChild>
+        <Link href="/dashboard">
+          <User className="mr-2 h-4 w-4" />
+          Dashboard
+        </Link>
+      </Button>
+    ) : (
+      <Button className="ml-4" variant="outline" asChild>
+        <Link href="/signup">Get Started</Link>
+      </Button>
+    )}
   </header>
 );
 
