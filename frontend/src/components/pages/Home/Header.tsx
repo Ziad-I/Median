29c2b5d0b8183120b61e180a7160d93221bef9d4
Ziad-I <68874104+Ziad-I@users.noreply.github.com>
2024-09-15 02:36:14 +0300

    fix hydration error by making buttons only load after hydration

--- frontend/src/components/pages/Home/Header.tsx
+++ frontend/src/components/pages/Home/Header.tsx
@@ -1,63 +1,18 @@
 "use client";
 
-import axios from "axios";
 import Link from "next/link";
-import { useState } from "react";
-import { useRouter } from "next/navigation";
-import { Feather, User, LogOut } from "lucide-react";
-import { Button } from "@/components/ui/button";
+import { Feather } from "lucide-react";
 import ThemeToggle from "./ThemeToggle";
-import { useAuthStore } from "@/providers/AuthStoreProvider";
-import { useToast } from "@/hooks/UseToast";
+import HeaderButtons from "./HeaderButtons";
 
 const Header = () => {
-  const { isLoggedIn, accessToken, logout } = useAuthStore((state) => state);
-  const [isLoggingOut, setIsLoggingOut] = useState(false);
-  const { toast } = useToast();
-  const router = useRouter();
-
-  const handleLogout = async () => {
-    setIsLoggingOut(true);
-    const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/logout`;
-
-    try {
-      const response = await axios.post(
-        url,
-        {},
-        {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        }
-      );
-
-      if (response.status === 200) {
-        logout();
-        router.push("/login");
-        toast({
-          title: "Logged out",
-          description: "You have successfully logged out.",
-        });
-      }
-    } catch (error) {
-      toast({
-        title: "Logout failed",
-        description: "Unable to log out. Please try again.",
-        variant: "destructive",
-      });
-      console.error(error);
-    } finally {
-      setIsLoggingOut(false);
-    }
-  };
-
   return (
     <header className="px-4 lg:px-6 h-14 flex items-center border-b">
       <Link className="flex items-center justify-center" href="/">
         <Feather className="h-6 w-6 text-primary" />
         <span className="sr-only">Median</span>
       </Link>
-      <nav className="ml-auto mr-5 flex gap-4 sm:gap-6">
+      <nav className="ml-auto flex items-center space-x-4">
         <Link
           className="text-sm font-medium hover:underline underline-offset-4"
           href="#"
@@ -76,39 +31,11 @@ const Header = () => {
         >
           Write
         </Link>
-        {!isLoggedIn && (
-          <Link
-            className="text-sm font-medium hover:underline underline-offset-4"
-            href="/login"
-          >
-            Sign In
-          </Link>
-        )}
+        <HeaderButtons />
       </nav>
-      <ThemeToggle />
-      {isLoggedIn ? (
-        <div>
-          <Button
-            variant="ghost"
-            className="ml-1"
-            onClick={handleLogout}
-            disabled={isLoggingOut}
-          >
-            <LogOut className="mr-2 h-4 w-4" />
-            {isLoggingOut ? "Logging out..." : "Logout"}
-          </Button>
-          <Button className="ml-4" variant="outline" asChild>
-            <Link href="/dashboard">
-              <User className="mr-2 h-4 w-4" />
-              Dashboard
-            </Link>
-          </Button>
-        </div>
-      ) : (
-        <Button className="ml-2" variant="outline" asChild>
-          <Link href="/signup">Get Started</Link>
-        </Button>
-      )}
+      <div className="ml-4">
+        <ThemeToggle />
+      </div>
     </header>
   );
 };
---
+++ frontend/src/components/pages/Home/HeaderButtons.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import axios from "axios";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { User, Rocket, LogOut, LogIn } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useAuthStore } from "@/providers/AuthStoreProvider";
+import { useToast } from "@/hooks/UseToast";
+
+const HeaderButtons = () => {
+  const [isHydrated, setIsHydrated] = useState(false);
+  const { isLoggedIn, accessToken, logout } = useAuthStore((state) => state);
+  const [isLoggingOut, setIsLoggingOut] = useState(false);
+  const { toast } = useToast();
+  const router = useRouter();
+
+  useEffect(() => {
+    setIsHydrated(true);
+  }, []);
+
+  const handleLogout = async () => {
+    setIsLoggingOut(true);
+    const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/logout`;
+
+    try {
+      const response = await axios.post(
+        url,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        logout();
+        router.push("/login");
+        toast({
+          title: "Logged out",
+          description: "You have successfully logged out.",
+        });
+      }
+    } catch (error) {
+      toast({
+        title: "Logout failed",
+        description: "Unable to log out. Please try again.",
+        variant: "destructive",
+      });
+      console.error(error);
+    } finally {
+      setIsLoggingOut(false);
+    }
+  };
+
+  if (!isHydrated) return null;
+
+  return (
+    <>
+      {!isLoggedIn ? (
+        <>
+          <Button variant="outline" asChild>
+            <Link href="/login">
+              <LogIn className="mr-2 h-4 w-4" />
+              Sign In
+            </Link>
+          </Button>
+          <Button variant="outline" asChild>
+            <Link href="/signup">
+              <Rocket className="mr-2 h-4 w-4" />
+              Get Started
+            </Link>
+          </Button>
+        </>
+      ) : (
+        <>
+          <Button
+            variant="outline"
+            onClick={handleLogout}
+            disabled={isLoggingOut}
+          >
+            <LogOut className="mr-2 h-4 w-4" />
+            Logout
+          </Button>
+          <Button variant="outline" asChild>
+            <Link href="/dashboard">
+              <User className="mr-2 h-4 w-4" />
+              Dashboard
+            </Link>
+          </Button>
+        </>
+      )}
+    </>
+  );
+};
+
+export default HeaderButtons;
