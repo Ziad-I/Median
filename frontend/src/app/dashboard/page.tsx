d4b98403a0f7be041ea6925d2b648c2c42a02b96
Ziad-I <68874104+Ziad-I@users.noreply.github.com>
2024-09-14 20:44:06 +0300

    fix double scrollbars in dashboard

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -13,62 +13,60 @@ import withAuth from "@/components/WithAuth";
 
 function DashboardPage() {
   return (
-    <div className="flex h-screen bg-background">
+    <div className="flex min-h-screen bg-background">
       {/* <Sidebar /> */}
-      <div className="flex flex-1 flex-col overflow-hidden">
-        <main className="flex-1 overflow-auto p-6">
-          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-            <StatCard
-              title="Total Views"
-              value="45,231"
-              description="+20.1% from last month"
-              icon={Eye}
-            />
-            <StatCard
-              title="Followers"
-              value="2,350"
-              description="+180 new followers"
-              icon={User}
-            />
-            <StatCard
-              title="Total Articles"
-              value="52"
-              description="+3 published this week"
-              icon={TrendingUp}
-            />
-            <StatCard
-              title="Total Comments"
-              value="1,429"
-              description="+20 new this week"
-              icon={MessageCircle}
-            />
-          </div>
+      <div className="flex-1 p-6">
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4 mb-6">
+          <StatCard
+            title="Total Views"
+            value="45,231"
+            description="+20.1% from last month"
+            icon={Eye}
+          />
+          <StatCard
+            title="Followers"
+            value="2,350"
+            description="+180 new followers"
+            icon={User}
+          />
+          <StatCard
+            title="Total Articles"
+            value="52"
+            description="+3 published this week"
+            icon={TrendingUp}
+          />
+          <StatCard
+            title="Total Comments"
+            value="1,429"
+            description="+20 new this week"
+            icon={MessageCircle}
+          />
+        </div>
 
-          <Tabs defaultValue="articles" className="mt-6">
-            <TabsList>
-              <TabsTrigger value="articles">Recent Articles</TabsTrigger>
-              <TabsTrigger value="comments">Recent Comments</TabsTrigger>
-              <TabsTrigger value="settings">User Settings</TabsTrigger>
-              <TabsTrigger value="followers">Followers</TabsTrigger>
-              <TabsTrigger value="following">Following</TabsTrigger>
-            </TabsList>
-            <TabsContent value="articles">
-              <ArticlesList />
-            </TabsContent>
-            <TabsContent value="comments">
-              <CommentsList />
-            </TabsContent>
-            <TabsContent value="settings">
-              <UserSettings />
-            </TabsContent>
-            <TabsContent value="followers">
-              <FollowersList />
-            </TabsContent>
-            <TabsContent value="following">
-              <FollowingList />
-            </TabsContent>
-          </Tabs>
-        </main>
+        <Tabs defaultValue="articles" className="mt-6">
+          <TabsList>
+            <TabsTrigger value="articles">Recent Articles</TabsTrigger>
+            <TabsTrigger value="comments">Recent Comments</TabsTrigger>
+            <TabsTrigger value="settings">User Settings</TabsTrigger>
+            <TabsTrigger value="followers">Followers</TabsTrigger>
+            <TabsTrigger value="following">Following</TabsTrigger>
+          </TabsList>
+          <TabsContent value="articles">
+            <ArticlesList />
+          </TabsContent>
+          <TabsContent value="comments">
+            <CommentsList />
+          </TabsContent>
+          <TabsContent value="settings">
+            <UserSettings />
+          </TabsContent>
+          <TabsContent value="followers">
+            <FollowersList />
+          </TabsContent>
+          <TabsContent value="following">
+            <FollowingList />
+          </TabsContent>
+        </Tabs>
       </div>
     </div>
   );
